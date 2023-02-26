8a08cb1189e770ba4ca001f3664df872c7ce83f6
Dmytro-Andrii Kostelnyi <dmitrokostelny95@gmail.com>
2023-02-26 16:06:36 +0200

    Write ImageService tests

--- TechStore.API.Tests/Services/ImageServiceTest.cs
+++ TechStore.API.Tests/Services/ImageServiceTest.cs
@@ -2,13 +2,16 @@
 using Moq;
 using TechStore.Web.Services;
 using System.IO.Abstractions.TestingHelpers;
+using System.IO.Abstractions;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 
 namespace TechStore.API.Tests.Services;
 
-internal class ImageServiceFixture : IDisposable
+public class ImageServiceFixture : IDisposable
 {
     public ImageService Service { get; private set; }
+    private IList<IFormFile> TestImages { get; set; }
 
     public ImageServiceFixture()
     {
@@ -32,6 +35,41 @@ internal class ImageServiceFixture : IDisposable
             .Returns("./");
 
         Service = new ImageService(optionsMock.Object, mockFs, envMock.Object);
+
+        TestImages = new List<IFormFile>();
+
+        var realFs = new FileSystem();
+
+        var testImagesPaths = realFs.Directory.EnumerateFiles("./TestImages");
+
+        var mimeMapping = new Dictionary<string, string>
+        {
+            { ".txt", "text/plain" },
+            { ".png", "image/png" }
+        };
+
+        foreach (var testImagePath in testImagesPaths)
+        {
+            using (var stream = realFs.File.OpenRead(testImagePath))
+            {
+                var testImageFileName = realFs.Path.GetFileName(testImagePath);
+                var testImageFileExtension = realFs.Path.GetExtension(testImagePath);
+                var testImageName = realFs.Path.GetFileNameWithoutExtension(testImagePath);
+
+                var imageFile = new FormFile(stream, 0, stream.Length, testImageName, testImageFileName)
+                {
+                    Headers = new HeaderDictionary(),
+                    ContentType = mimeMapping[testImageFileExtension]
+                };
+
+                TestImages.Add(imageFile);
+            }
+        }
+    }
+
+    public IFormFile GetTestImage(string imageName)
+    {
+        return TestImages.Where(imageFile => imageFile.Name == imageName).First();
     }
 
     public void Dispose()
@@ -39,7 +77,7 @@ internal class ImageServiceFixture : IDisposable
     }
 }
 
-internal class ImageServiceTest : IClassFixture<ImageServiceFixture>
+public class ImageServiceTest : IClassFixture<ImageServiceFixture>
 {
     private readonly ImageServiceFixture _fixture;
 
@@ -48,5 +86,27 @@ internal class ImageServiceTest : IClassFixture<ImageServiceFixture>
         _fixture = fixture;
     }
 
-    // TODO: Write tests
+    [Fact]
+    public void CheckImageFilesNoImagesThrows()
+    {
+        Assert.Throws<ArgumentException>(() =>
+        {
+            _fixture.Service.CheckImageFiles(new FormFileCollection());
+        });
+    }
+
+    [Fact]
+    public void CheckImageFileInvalidContentTypeThrows()
+    {
+        var testImage = _fixture.GetTestImage("NotImage");
+        var fileCollection = new FormFileCollection
+        {
+            testImage
+        };
+
+        Assert.Throws<AggregateException>(() =>
+        {
+            _fixture.Service.CheckImageFiles(fileCollection);
+        });
+    }
 }
